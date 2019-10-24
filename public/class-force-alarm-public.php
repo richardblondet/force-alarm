6df1af6e8675978a877bda7bb48cf413e62d5dca
richardblondet <richardblondet@gmail.com>
2019-10-24 17:26:11 -0400

    line deb

--- public/class-force-alarm-public.php
+++ public/class-force-alarm-public.php
@@ -253,10 +253,10 @@ class Force_Alarm_Public {
 		try {
 			// $available 					= $this->validate_service_installation_availability( $data );
 			// $payment_response 	= $this->fa_order_process_payment( $data );
-			$user_id		 				= $this->fa_order_process_user( $data );
-			// $order_id						= $this->fa_order_process_cart( $user_id, $data );
+			// $user_id		 				= $this->fa_order_process_user( $data );
+			$order_id						= $this->fa_order_process_cart( 24, $data );
 			
-			return wp_send_json_error( $user_id, 500 ); // Test
+			return wp_send_json_error( $order_id, 500 ); // Test
 			$response['payment_response'] = $payment_response;
 			$response['user_id'] 					= $user_id;
 			$response['order_id'] 				= $order_id;
