003fa25433a9399a867f79a612a020e5c96ccb7f
richardblondet <richardblondet@gmail.com>
2019-10-24 15:51:55 -0400

    line deb

--- public/class-force-alarm-public.php
+++ public/class-force-alarm-public.php
@@ -349,9 +349,9 @@ class Force_Alarm_Public {
 		
 		// Verify error on service communication and throw Exception
 		if( is_wp_error( $response )) { // $res->get_error_message()
+			return wp_send_json_error( [$response, __LINE__], 500 );
 			throw new Exception('Ha ocurrido un fallo en la comunicación en la terminal de pago.');
 		}
-		return wp_send_json_error( [$response, __LINE__], 500 );
 		$response['request'] = $request;
 		$response['url'] = esc_url_raw( $url );
 		// Decode body
